0795b73dd48d11bc75024a3faa5bc8fea3e69cc2
atharva <atharva.badhan@gmail.com>
2023-08-05 17:45:33 +0530

    1st Version

---
+++ src/pages/SkittiWebsite.js
@@ -0,0 +1,139 @@
+import styles from "./SkittiWebsite.module.css";
+
+
+const SkittiWebsite = () => {
+  return (
+    <div className={styles.skittiWebsite}>
+      <div className={styles.rectangleParent}>
+        <div className={styles.frameChild} />
+        <div className={styles.rectangleGroup}>
+          <div className={styles.groupChild} />
+          <div className={styles.getStarted}>Get Started</div>
+        </div>
+        <div className={styles.rectangleContainer}>
+          <img className={styles.groupItem} alt="" src="/rectangle-2@2x.png" />
+          <div className={styles.improveAcademics}>{`Improve Academics `}</div>
+          <div className={styles.evenTheBrightestContainer}>
+            <span className={styles.liveQuizTxtContainer}>
+              <p className={styles.evenTheBrightest}>
+                Even the brightest students can sometimes find themselves
+                academically underperforming, often through no fault of their
+                own.
+              </p>
+              <p className={styles.evenTheBrightest}>
+                It's often because they're stuck in a rut and are not sure what
+                to do to improve.
+              </p>
+            </span>
+          </div>
+          <div className={styles.groupDiv}>
+            <div className={styles.groupInner} />
+            <div className={styles.download}>Download</div>
+          </div>
+          <img
+            className={styles.materialSymbolsdownloadIcon}
+            alt=""
+            src="/materialsymbolsdownload.svg"
+          />
+        </div>
+        <div className={styles.unleashYourFull}>
+          Unleash your full potential with Skitti.
+        </div>
+        <div className={styles.learnBetterFocus}>
+          Learn better, focus harder, stress less
+        </div>
+        <div className={styles.createAPersonalized}>
+          create a personalized study schedule and track Your progress
+        </div>
+        <b className={styles.whetherYoureFeeling}>
+          whether you're feeling angry, sad, motivated, or anything in between,
+          Skitti can help you stay on track and achieve your goals.
+        </b>
+        <div className={styles.reachYourLearning}>
+          Reach your learning goals
+        </div>
+        <div className={styles.skitiiParent}>
+          <div className={styles.skitii}>Skitii</div>
+          <img
+            className={styles.svg2023031622244600001Icon}
+            alt=""
+            src="/svg-20230316-222446-0000-1.svg"
+          />
+        </div>
+        <div className={styles.frameItem} />
+        <div className={styles.hostsLiveQuizzesThatYouCaParent}>
+          <div className={styles.hostsLiveQuizzes}>
+            hosts live quizzes that you can compete in against other users. This
+            is a great way to test your knowledge, have fun, and earn rewards.
+          </div>
+          <div className={styles.learnMore}>{`Learn More  >`}</div>
+          <div className={styles.liveQuiz}>
+            <span className={styles.liveQuizTxtContainer}>
+              <span>Live</span>
+              <span className={styles.span}>{` `}</span>
+              <span className={styles.quiz}>Quiz</span>
+            </span>
+          </div>
+        </div>
+        <div className={styles.rectangleParent1}>
+          <img className={styles.groupItem} alt="" src="/rectangle-21@2x.png" />
+          <div className={styles.rectangleDiv} />
+          <div className={styles.theStudyPattern}>
+            The Study Pattern Analysis feature analyzes your study pattern and
+            provides recommendations on how you can improve.
+          </div>
+          <div className={styles.studyPatternAnalysis}>
+            Study Pattern Analysis
+          </div>
+          <div className={styles.learnMore1}>{`Learn More  >`}</div>
+        </div>
+        <img className={styles.bitmapIcon} alt="" src="/bitmap@2x.png" />
+        <img className={styles.bitmapIcon1} alt="" src="/bitmap1@2x.png" />
+        <img className={styles.bitmapIcon2} alt="" src="/bitmap2@2x.png" />
+        <img className={styles.bitmapIcon3} alt="" src="/bitmap3@2x.png" />
+        <img className={styles.bitmapIcon4} alt="" src="/bitmap4@2x.png" />
+        <img className={styles.bitmapIcon5} alt="" src="/bitmap5@2x.png" />
+        <img className={styles.frameInner} alt="" src="/group-4.svg" />
+        <div className={styles.rectangleParent2}>
+          <div className={styles.groupChild1} />
+          <div className={styles.english}>English</div>
+          <img
+            className={styles.iconArrowIosDownward}
+            alt=""
+            src="/-icon-arrow-ios-downward.svg"
+          />
+        </div>
+        <div className={styles.component2Wrapper}>
+          <div className={styles.component2}>
+            <img
+              className={styles.scheduleScreen1}
+              alt=""
+              src="/schedule-screen-1@2x.png"
+            />
+          </div>
+        </div>
+        <div className={styles.component1Wrapper}>
+          <div className={styles.component2}>
+            <img
+              className={styles.scheduleScreen1}
+              alt=""
+              src="/live-quiz-join-1@2x.png"
+            />
+          </div>
+        </div>
+      </div>
+      <img
+        className={styles.iongameControllerOutlineIcon}
+        alt=""
+        src="/iongamecontrolleroutline.svg"
+      />
+      <img
+        className={styles.akarIconstrophy}
+        alt=""
+        src="/akariconstrophy.svg"
+      />
+    </div>
+  );
+};
+
+export default SkittiWebsite;
